e3bc86eb44790c197cfa4acf177ffb798864dd5f
MyPoZi <kxx555u.u@gmail.com>
2019-11-22 11:48:58 +0900

    add 複数画像送信のmethods

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,13 @@
       <form @submit.prevent="submit">
         <label>
           画像
-          <input type="file" @change="onFileChange" />
+          <input
+            id="post_images"
+            ref="image"
+            @change="onFileChange"
+            type="file"
+            multiple="multiple"
+          />
         </label>
         <button type="submit" class="button button--inverse">submit</button>
       </form>
@@ -13,28 +19,50 @@
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
   data() {
     return {
-      photo: null
+      photo: null,
+      images: []
     }
   },
   methods: {
     async submit() {
+      const params = {
+        text: 'text_text',
+        images: this.images
+      }
       const formData = new FormData()
-      formData.append('photo', this.photo)
+      // eslint-disable-next-line no-console
+      console.log(this.images)
+      Object.entries(params).forEach(([key, value]) => {
+        if (Array.isArray(value)) {
+          value.forEach((v, i) => {
+            formData.append(key + '[]', v) // arrayデータを分割して入れ直す
+          })
+        } else {
+          formData.append(key, value)
+        }
+      })
+      // for (let i = 0; i < this.images.length; i++) {
+      //   // const name = 'uploadedImages[' + i + ']'
+      //   const name = 'images'
+      //   console.log(this.images[i])
+      //   formData.append(name, this.images)
+      //   // formData.append(name, JSON.stringify(this.images))
+      // }
       // eslint-disable-next-line no-console
       console.log(formData)
+      // formData.append('images', this.images[0])
+      // formData.append('images', this.images[1])
       // eslint-disable-next-line no-console
-      console.log(this.photo)
-      const response = await axios.post('api/v1/posts', formData)
+      const response = await this.$axios.post('/posts', formData)
       // eslint-disable-next-line no-console
       console.log(response)
     },
     onFileChange(event) {
-      this.photo = event.target.files[0]
+      const files = event.target.files || event.dataTransfer.files
+      this.images.push(files[0])
     }
   }
 }
