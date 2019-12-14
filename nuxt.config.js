5a487ad05c0b928d7f1d8e97d067a3fb5d226106
MyPoZi <kxx555u.u@gmail.com>
2019-12-14 12:39:28 +0900

    test target pathrewrite 変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
     '/api': {
       target: 'https://oshi-loss-api.herokuapp.com',
       pathRewrite: {
-        '^/api': '/api/v1'
+        '^/api': 'https://oshi-loss-api.herokuapp.com/api/v1'
       }
     }
   },
